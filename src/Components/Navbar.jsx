2788c0d3728d575aaa204b65e6ab4a412d274bcb
Abr009 <bradithya945@gmail.com>
2025-02-05 20:56:48 +0530

    Updated Navbar.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
   return (
     <nav className="w-[100%] h-[10vh] bg-black text-white fixed top-0 left-0 flex items-center justify-between px-6 font-gochi">
       <div>
-        <h1 class="md:text-5xl text-[2.8rem] tracking-wider font-semibold">
+        <h1 class="md:text-5xl text-[2.8rem] tracking-wider font-semibold hover:scale-105 transition-all duration-500 cursor-pointer">
           <span class="text-lime-500">E</span>
           <span class="text-green-700">c</span>
           <span class="text-yellow-500">h</span>
@@ -25,11 +25,11 @@ export const Navbar = () => {
       </div>
 
       <div className="gap-8 text-2xl hidden md:flex">
-        <a className="tracking-wide hover:text-lime-500 transition-all duration-300" href="#home">Home</a>
-        <a className="tracking-wide hover:text-lime-500 transition-all duration-300" href="#about">About</a>
-        <a className="tracking-wide hover:text-lime-500 transition-all duration-300" href="#events">Events</a>
-        <a className="tracking-wide hover:text-lime-500 transition-all duration-300" href="#rules">Rules</a>
-        <a className="tracking-wide hover:text-lime-500 transition-all duration-300" href="#contact">Contact</a>
+        <a className="tracking-wide hover:text-lime-500 hover:scale-110 transition-all duration-300" href="#home">Home</a>
+        <a className="tracking-wide hover:text-lime-500 hover:scale-110 transition-all duration-300" href="#about">About</a>
+        <a className="tracking-wide hover:text-lime-500 hover:scale-110 transition-all duration-300" href="#events">Events</a>
+        <a className="tracking-wide hover:text-lime-500 hover:scale-110 transition-all duration-300" href="#rules">Rules</a>
+        <a className="tracking-wide hover:text-lime-500 hover:scale-110 transition-all duration-300" href="#contact">Contact</a>
       </div>
 
       <i class='bx bx-menu text-4xl md:hidden' onClick={() => setDisplay(!display)}></i>
