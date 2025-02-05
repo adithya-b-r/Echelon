561d8c6489beee1a349acf3a82087f455832416f
Abr009 <bradithya945@gmail.com>
2025-02-05 21:24:09 +0530

    Updated Home page and global CSS.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 export const Home = () => {
   return (
-    <div id="home" className="w-full h-[90vh] bg-transparent flex items-center justify-center scroll-mt-[10vh]">
-      <h1 className="font-extrabold text-4xl text-white">Home</h1>
+    <div id="home" className="w-full h-[90vh] bg-transparent flex flex-col items-center justify-center scroll-mt-[10vh]">
+      <img className="h-[45%] w-fit brightness-125 drop-shadow-lg" src="imgs/logo.png" alt="" />
+
+      <div></div>
+
+      <button className="bg-blue-500 rounded-md w-[70%] h-12 font-gochi text-3xl text-white mt-10">
+        Register Now
+      </button>
     </div>
   )
 }
