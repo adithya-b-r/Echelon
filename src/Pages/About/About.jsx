f1d787366eee5bc572978e75e1a27e1ed2dff8fc
Abr009 <bradithya945@gmail.com>
2025-02-07 12:49:41 +0530

    Added about Sectio.

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,25 @@
 export const About = () => {
   return (
-    <div id="about" className="w-full h-[90vh] bg-transparent flex items-center justify-center scroll-mt-[10vh]">
-      <h1 className="font-extrabold text-4xl text-white">About</h1>
+    <div id="about" className="w-full h-fit bg-transparent text-white font-gochi">
+      <div className="w-full px-10 md:px-32">
+        <div
+          style={{ border: "5px ridge lime", backgroundColor: "rgba(0, 0, 0, 0.7)" }}
+          className=" p-6 rounded-lg"
+        >
+          <h1 className="capitalize text-center font-semibold text-3xl md:text-5xl mb-6 tracking-widest">About College</h1>
+          <p className="md:text-2xl tracking-wide text-xl ">
+            Shree Devi Institute of Technology, Kenjar, Mangaluru, affiliated with Visvesvaraya Technological University, Belagavi, and recognized by the All India Council for Technical Education (AICTE), New Delhi, was established by Shree Devi Education Trust, Mangaluru, in the year 2006. Our institution aims to provide students with a rich and prosperous learning environment so that they acquire the knowledge and skills needed to succeed in their professional as well as personal lives.
+            
+            This institute offers students the opportunity to pursue engineering, management, and computer studies in an ambiance of natural beauty. A team of dedicated faculty, a brilliant student community, excellent technical and supporting staff, and an effective administration have all contributed to the pre-eminent status of Shree Devi Institute of Technology.
+          </p>
+
+          <h1 className="capitalize text-center font-semibold text-3xl md:text-5xl mb-6 tracking-widest mt-14">about echelon</h1>
+          <p className="md:text-2xl tracking-wide text-xl ">
+            A national-level management and cultural fest organized by Shree Devi institute of technology under the banner of Meraki Association is scheduled to be on February 28th &1st March 2025 at Shree Devi Institute of Technology campus, Airport road, Kenjar, Mangaluru. 
+            Echelon 25, A National-Level Management and Culture Fest; it is a platform dedicated to driving meaningful change and advancing the global agenda set by the Sustainable Development Goals (SDGs). With 17 interconnected goals, from eradicating poverty and achieving gender equality to fostering climate action and promoting peace, Echelon 25 encourages participants to explore, innovate, and collaborate towards creating a sustainable future. The fest aims to ignite conversations, inspire creativity, and foster solutions that contribute to a more equitable world. By uniting young minds across the nation, we strive to make a collective impact, working together to achieve the SDGs and leave a legacy of positive transformation for generations to come.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
