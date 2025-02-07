97e373853d2529dd54e191224b385946967755d0
Abr009 <bradithya945@gmail.com>
2025-02-07 12:06:33 +0530

    Added responsive for Contact section.

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -14,7 +14,7 @@ export const Contact = () => {
         </div>
       </div>
 
-      <div className="flex flex-col md:w-[30%] w-[100%] md:ml-24 ml-18 leading-relaxed">
+      <div className="flex flex-col md:w-[30%] w-[100%] md:ml-24 leading-relaxed md:text-left text-center">
         <h3 className="md:mt-4 mb-4 font-bold text-2xl">Quick Links</h3>
         <a className="tracking-wide hover:text-lime-500 hover:scale-110 transition-all duration-300" href="#home">Home</a>
         <a className="tracking-wide hover:text-lime-500 hover:scale-110 transition-all duration-300" href="#about">About</a>
@@ -23,14 +23,14 @@ export const Contact = () => {
         <a className="tracking-wide hover:text-lime-500 hover:scale-110 transition-all duration-300" href="#contact">Contact</a>
       </div>
 
-      <div className="flex w-[33%] h-[30vh] bg-green-400 rounded-lg overflow-hidden">
-        <iframe 
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31106.467602504323!2d74.83182507431643!3d12.9521038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba350d45819abf1%3A0xec9cf13e2e0ad60c!2sShree%20Devi%20Institute%20Of%20Technology!5e0!3m2!1sen!2sin!4v1738907515444!5m2!1sen!2sin" 
-          width="100%" 
-          height="100%" 
-          style={{ border: 0, borderRadius: "8px", overflow: "hidden" }} 
-          allowFullScreen="" 
-          loading="lazy" 
+      <div className="flex md:w-[33%] w-[100%] h-[30vh] bg-green-400 rounded-lg overflow-hidden md:mt-0 mt-8">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31106.467602504323!2d74.83182507431643!3d12.9521038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba350d45819abf1%3A0xec9cf13e2e0ad60c!2sShree%20Devi%20Institute%20Of%20Technology!5e0!3m2!1sen!2sin!4v1738907515444!5m2!1sen!2sin"
+          width="100%"
+          height="100%"
+          style={{ border: 0, borderRadius: "8px", overflow: "hidden" }}
+          allowFullScreen=""
+          loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
