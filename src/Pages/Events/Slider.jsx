5ac60e6ee71982d819b9a11342f7614741a0041e
Adithya B R <59525997+adithya-b-r@users.noreply.github.com>
2025-02-16 22:56:05 +0530

    Update Slider.jsx

--- src/Pages/Events/Slider.jsx
+++ src/Pages/Events/Slider.jsx
@@ -28,7 +28,7 @@ export default function Slider({ start = 0, end = 3, type = 0 }) {
     ["SPECTRA", "2", "Shreevatsa - 8660049762", "Sushmitha Amin - 9834063924", "BRAND RANGOLI"], //pg & ug
     ["ZENBLAZE", "4-8", "Prajna Kundar - 9110464528", "Raksha T - 9945138003", "CORPORATE WALK"], //
     ["AURA", "1", "Prakruthiji - 8277091324", "SPOT DANCE"],
-    ["HIDDEN TRAIL", "2", "Akhilesh - 8105456871", "Puneeth G Mallaya - 8792009632", "TREASURE HUNT"],
+    ["HIDDEN TRAIL", "2", "Akhilesh - 8105456871", "Puneeth G Mallya - 8792009632", "TREASURE HUNT"],
     ["AD-VENGERS", "5", "Nishmitha - 7619531629", "Shruthi - 8431742695", "MAD AD"],
     ["IRIS", "2", "Vishnu Ashok - 7034069185", "Naijin Johny - 9380727878", "PHOTOGRAPHY"],
     ["CINEQUEST", "2", "Hisham Hussain - 7338549200", "Abhin - 9731445371", "MOVIE QUIZ"],
